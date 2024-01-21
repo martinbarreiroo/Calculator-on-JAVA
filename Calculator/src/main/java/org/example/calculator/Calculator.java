bad73ff00fb49e495a6f2883d0e421c1e1abb4ea
martinbarreiroo <mbarreiro@mail.austral.edu.ar>
2024-01-21 20:37:30 -0300

    13 commit

--- Calculator/src/main/java/org/example/calculator/Calculator.java
+++ Calculator/src/main/java/org/example/calculator/Calculator.java
@@ -9,6 +9,7 @@ import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -18,77 +19,69 @@ import java.util.Objects;
 public class Calculator extends Application {
 
     private TextField display;
+    private boolean enterPressed = false;
 
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Calculator");
 
         // Create buttons for digits and operations
-        Button[][] buttons = new Button[5][4];  // Changed array size to accommodate "AC"
+        Button[][] buttons = new Button[5][4];
         String[][] buttonLabels = {
                 {"7", "8", "9", "←"},
                 {"4", "5", "6", "*"},
                 {"1", "2", "3", "-"},
                 {"0", ".", "=", "+"},
-                {"√", "²", "/", "AC"}  // Ensure each row has the same length
+                {"√", "²", "/", "AC"}
         };
 
-        // Create a GridPane for layout
         GridPane gridPane = new GridPane();
         gridPane.setPadding(new Insets(10, 10, 10, 10));
         gridPane.setVgap(5);
         gridPane.setHgap(5);
         gridPane.getStyleClass().add("grid-pane");
 
-        // Set ColumnConstraints to make buttons resizable
         ColumnConstraints columnConstraints = new ColumnConstraints();
         columnConstraints.setPercentWidth(25);
         for (int i = 0; i < 4; i++) {
             gridPane.getColumnConstraints().add(columnConstraints);
         }
 
-        // Add the display to the layout, spanning across all columns
         display = new TextField();
         display.setEditable(false);
         display.setMinSize(200, 30);
         display.setMaxSize(Double.MAX_VALUE, 100);
         GridPane.setColumnSpan(display, 4);
-
-        // Set Vgrow property to SOMETIMES, making the display take only the required vertical space
         GridPane.setVgrow(display, Priority.SOMETIMES);
-
         gridPane.add(display, 0, 0);
 
-        // Make the scene resizable
         primaryStage.widthProperty().addListener((obs, oldVal, newVal) -> {
-            double fontSize = newVal.doubleValue() / 15; // Adjust this value according to your preference
+            double fontSize = newVal.doubleValue() / 15;
             display.setStyle("-fx-font-size: " + fontSize + "px;");
             gridPane.setPrefWidth(newVal.doubleValue());
-            display.setPrefWidth(newVal.doubleValue() - 20); // Adjust for padding
+            display.setPrefWidth(newVal.doubleValue() - 20);
         });
 
         primaryStage.heightProperty().addListener((obs, oldVal, newVal) -> {
-            double fontSize = newVal.doubleValue() / 15; // Adjust this value according to your preference
+            double fontSize = newVal.doubleValue() / 15;
             display.setStyle("-fx-font-size: " + fontSize + "px;");
             gridPane.setPrefHeight(newVal.doubleValue());
-            display.setPrefHeight(newVal.doubleValue() - 40); // Adjust for padding
+            display.setPrefHeight(newVal.doubleValue() - 40);
         });
 
-        // Add buttons to the gridPane with fade-in animation
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 4; j++) {
                 buttons[i][j] = new Button(buttonLabels[i][j]);
                 buttons[i][j].setMinSize(50, 50);
-                buttons[i][j].setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE); // Allow buttons to be resizable
-                GridPane.setHgrow(buttons[i][j], Priority.ALWAYS); // Allow buttons to grow horizontally
-                GridPane.setVgrow(buttons[i][j], Priority.ALWAYS); // Allow buttons to grow vertically
-                GridPane.setFillWidth(buttons[i][j], true); // Allow buttons to fill the available width
-                GridPane.setFillHeight(buttons[i][j], true); // Allow buttons to fill the available height
-                GridPane.setHalignment(buttons[i][j], HPos.CENTER); // Center the button horizontally
-                GridPane.setValignment(buttons[i][j], VPos.CENTER); // Center the button vertically
-                gridPane.add(buttons[i][j], j, i + 1);  // Increase the row index by 1
+                buttons[i][j].setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+                GridPane.setHgrow(buttons[i][j], Priority.ALWAYS);
+                GridPane.setVgrow(buttons[i][j], Priority.ALWAYS);
+                GridPane.setFillWidth(buttons[i][j], true);
+                GridPane.setFillHeight(buttons[i][j], true);
+                GridPane.setHalignment(buttons[i][j], HPos.CENTER);
+                GridPane.setValignment(buttons[i][j], VPos.CENTER);
+                gridPane.add(buttons[i][j], j, i + 1);
 
-                // Create a FadeTransition for each button
                 FadeTransition fadeTransition = new FadeTransition(Duration.millis(500), buttons[i][j]);
                 fadeTransition.setFromValue(0);
                 fadeTransition.setToValue(1);
@@ -96,13 +89,9 @@ public class Calculator extends Application {
             }
         }
 
-
-
-        // Set actions for the buttons
         setButtonActions(buttons);
 
-        // Set up the scene
-        Scene scene = new Scene(gridPane, 250, 350);  // Adjust the scene height
+        Scene scene = new Scene(gridPane, 250, 350);
         scene.getStylesheets().add(Objects.requireNonNull(
                 getClass().getResource("/org/example/calculator/styles.css")
         ).toExternalForm());
@@ -122,103 +111,130 @@ public class Calculator extends Application {
             }
         }
 
-        // Make the scene resizable
         primaryStage.widthProperty().addListener((obs, oldVal, newVal) -> {
             gridPane.setPrefWidth(newVal.doubleValue());
-            display.setPrefWidth(newVal.doubleValue() - 20); // Adjust for padding
+            display.setPrefWidth(newVal.doubleValue() - 20);
         });
 
         primaryStage.heightProperty().addListener((obs, oldVal, newVal) -> {
             gridPane.setPrefHeight(newVal.doubleValue());
-            display.setPrefHeight(newVal.doubleValue() - 40); // Adjust for padding
+            display.setPrefHeight(newVal.doubleValue() - 40);
         });
 
-        primaryStage.show();
-    }
+        // Add keyboard input handling
+        display.setOnKeyPressed(this::handleKeyPressed);
 
+        primaryStage.show();
+
+        display.requestFocus();
+    }
 
     private void setButtonActions(Button[][] buttons) {
         for (Button[] buttonRow : buttons) {
             for (Button button : buttonRow) {
-                if (button != null) {  // Check if the button is not null
+                if (button != null) {
                     button.setOnAction(e -> handleButtonClick(button.getText()));
                 }
             }
         }
     }
 
-
     private void handleButtonClick(String value) {
-        // Handle button click and update the display
         if ("=".equals(value)) {
             calculateResult();
+            enterPressed = false; // Reset the flag when the "=" button is pressed
         } else if ("AC".equals(value)) {
             clearDisplay();
         } else if ("←".equals(value)) {
             handleBackspaceButtonClick();
-        }
-        else {
+        } else {
             display.appendText(value);
         }
     }
 
+    private void handleKeyPressed(KeyEvent event) {
+        String keyText = event.getText();
+
+        if (!keyText.isEmpty()) {
+            if (event.isShiftDown()) {
+                handleShiftedKey(keyText);
+            } else {
+                handleButtonClick(keyText);
+            }
+            enterPressed = false; // Reset the flag when any other key is pressed
+        } else {
+            switch (event.getCode()) {
+                case ENTER:
+                    if (!enterPressed) {
+                        calculateResult();
+                        enterPressed = true; // Set the flag to true after the calculation
+                    }
+                    break;
+                case BACK_SPACE:
+                    handleBackspaceButtonClick();
+                    break;
+                // Add more cases as needed for other special keys
+            }
+        }
+    }
+
     private void calculateResult() {
-        // Calculate the result and update the display
         try {
             String expression = display.getText();
-            System.out.println("Expression: " + expression); // Debug print
-
-            // Check if the expression contains the square root symbol
             if (expression.contains("√")) {
                 double result = calculateSquareRoot(expression);
                 display.setText(String.valueOf(result));
-            }
-            else if (expression.contains("²")){
-                double result =  calculateSquare(expression);
+            } else if (expression.contains("²")) {
+                double result = calculateSquare(expression);
                 display.setText(String.valueOf(result));
-            }
-
-            else {
-                // Evaluate the expression for other operations
+            } else {
                 double result = eval(expression);
-                System.out.println("Result: " + result); // Debug print
                 display.setText(String.valueOf(result));
             }
         } catch (Exception e) {
-            e.printStackTrace(); // Debug print
+            e.printStackTrace();
             display.setText("Error");
         }
     }
 
     private double calculateSquareRoot(String expression) {
-        // Remove the square root symbol and parse the remaining expression
         String expressionWithoutSqrt = expression.replace("√", "");
         double operand = eval(expressionWithoutSqrt);
-
-        // Calculate the square root
         return Math.sqrt(operand);
     }
 
     private double calculateSquare(String expression) {
-        // Remove the square root symbol and parse the remaining expression
         String expressionWithoutSq = expression.replace("²", "");
         double operand = eval(expressionWithoutSq);
-
-        // Calculate the square root
-        return Math.pow(operand,2);
+        return Math.pow(operand, 2);
     }
 
-
     private void handleBackspaceButtonClick() {
-        // Handle backspace button click and update the display
         String currentText = display.getText();
         if (!currentText.isEmpty()) {
             display.setText(currentText.substring(0, currentText.length() - 1));
         }
     }
 
+    private void handleShiftedKey(String keyText) {
+        // Handle SHIFT + key combinations
+        switch (keyText) {
+            case "7":
+                display.appendText("/");
+                break;
+
+            case "+":
+
+                display.appendText("*");
+                break;
+            case "0":
+
+                display.appendText("=");
+                break;
+        }
+    }
+
     private void clearDisplay() {
-        // Clear the display
         Platform.runLater(() -> display.setText(""));
     }
 
